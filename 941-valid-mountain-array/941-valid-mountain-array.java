a81e376cafbce77063076cd09a05cdfeb612dd8f
alarahergun <57047231+alarahergun@users.noreply.github.com>
2022-08-07 23:46:19 +0300

    Time: 3 ms (31.56%), Space: 52.8 MB (84.24%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -0,0 +1,48 @@
+class Solution {
+    public static boolean validMountainArray(int[] arr) {
+        boolean increase = false;
+        boolean decrease = false;
+        int value = arr[0];
+
+        for(int i=1; i<arr.length; i++) {
+
+            // in the beginning it has to increase so if it fails we stop
+            if(i==1) {
+                if(arr[i]>value) {
+                    increase = true;
+                    value = arr[i];
+                } else {
+                    return false;
+                }
+            } else {
+                if(increase) {
+                    if (arr[i] > value) {
+                        value = arr[i];
+                    } else {
+                        if (arr[i] == value) {
+                            return false;
+                        } else {
+                            increase = false;
+                            decrease = true;
+                            value = arr[i];
+                        }
+                    }
+                } else {
+                    if (decrease) {
+                        if (arr[i] >= value) {
+                            return false;
+                        } else {
+                            value = arr[i];
+                        }
+                    }
+                }
+            }
+        }
+        
+        if(!decrease){
+            return false;
+        }
+
+        return true;
+    }
+}
