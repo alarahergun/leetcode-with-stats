d45a9253c12e83fd460e43e2df2bb89ac961098b
alarahergun <57047231+alarahergun@users.noreply.github.com>
2022-10-14 17:02:23 +0300

    Time: 30 ms (22.59%), Space: 68.4 MB (66.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-      for(int i=0; i<nums.length-1; i++){
-        if(nums[i] == nums[i+1]){
-          return true;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<nums.length; i++) {
+            if(!set.add(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
         }
-      }
-
-      return false;
+        
+        return false;
+        
     }
 }
