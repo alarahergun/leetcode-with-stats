7861cb528fac5c49b2c514b6b14d1e2d220821db
alarahergun <57047231+alarahergun@users.noreply.github.com>
2022-11-11 19:17:04 +0300

    Time: 2 ms (35.93%), Space: 42.9 MB (7.13%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        // the same question with rotate image but rotating 3 times more
+        if(Arrays.deepEquals(mat, target)) {
+            return true;
+        }
+        
+        for(int i=0; i<4; i++) {
+            rotateImage(mat);
+            if(Arrays.deepEquals(mat, target)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public void rotateImage(int[][] matrix) {
+        int n = matrix.length;
+        
+        // transposing + reversing = 90 degrees rotation
+        
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            } 
+        }
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n/2; j++) {
+                int temp = matrix[i][n-j-1];
+                matrix[i][n-j-1] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
