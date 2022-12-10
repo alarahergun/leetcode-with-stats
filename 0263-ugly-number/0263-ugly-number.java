ee9bc0bae78890ddb671d9726efb86c0214d8c0c
alarahergun <57047231+alarahergun@users.noreply.github.com>
2022-12-11 01:02:36 +0300

    Time: 2 ms (79.22%), Space: 41.5 MB (35.33%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isUgly(int n) {
+        
+
+        while(n>1) {
+             if(n%5 == 0) {
+                n /= 5;
+             } else if(n%3 == 0) {
+                 n /= 3;
+             } else if(n%2 == 0) {
+                 n /= 2;
+             } else return false;
+        }
+        
+        return n > 0;
+    }
+}
