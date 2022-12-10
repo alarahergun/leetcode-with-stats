0a702a181ff0476e20489d45de5c9b8f4e17c37d
alarahergun <57047231+alarahergun@users.noreply.github.com>
2022-12-11 01:03:58 +0300

    Time: 1 ms (100.00%), Space: 39.4 MB (90.94%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isUgly(int n) {
         
+        // for this question if 1 is obtained until all divisions everything is fine
+        // if any number bigger than 1 is obtained it is a prime other than 2 3 or 5
 
         while(n>1) {
              if(n%5 == 0) {
